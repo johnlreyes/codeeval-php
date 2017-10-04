9c76146767c4e71d9c2c62bd9ab5587b71d23216
John Reyes <johnlreyes@yahoo.com>
2017-10-05 03:23:04 +0800

    107

---
+++ codes/src/challenges/easy/_107_shortest_repetition/Code.php
@@ -0,0 +1,25 @@
+<?php
+    namespace codes\src\challenges\easy\_001_fizz_buzz;
+
+    if (isset($argv)) {
+        $lines = file($argv[1]);
+        $code = new Code();
+        echo $code->execute($lines);
+    }
+
+    class Code {
+    
+        function execute($inputList) {
+            $resultStringArr = array();
+            foreach ($inputList as $index => $input) {
+                array_push($resultStringArr, $this->process(trim($input)));
+            }
+            return implode("\n", $resultStringArr);
+        }
+        
+        function process($line) {
+            $nextIndexOfFirstChar = strpos($line, $line[0], 1);
+            return $nextIndexOfFirstChar === false ? strlen($line) : $nextIndexOfFirstChar;
+        }
+    }
+?>
